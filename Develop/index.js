10311a2e8d66badb281928c75ea18345e8078c0d
bengallagher <bgallag5@gmail.com>
2021-07-18 08:24:16 -0400

    generates readme.md with answers to questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,45 @@
+//import packages; array of questions; init() => generateMarkdown 
+
+
+
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const writeToFile = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: title => {
+        if (title){
+            return true;
+        } else if(!title){
+            console.log('Please enter your name');
+            return false;
+        }
+    }
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(questions =>{
+
+    const markupHtml = writeToFile(questions);
+    fs.writeFile("./dist/README.md", markupHtml, err =>{
+        if(err){
+            console.log(err);
+        } else{
+            console.log('Its working its working!!');
+        }
+    })
+    })
+}
 
 // Function call to initialize app
 init();
